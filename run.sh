2e350387125c532cb476a88374dfddabac385b75
eager7 <panchagntao@gmail.com>
2016-09-08 19:50:54 -0700

    run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
-sudo ./wpa_supplicant -Dnl80211,wext -i wlxe84e060a11bd -c examples/wpa-psk-tkip.conf -d
+
+target=wpa_supplicant-2.5
+
+build()
+{
+    if [ ! -f ./build/wpa_supplicant ];then
+        echo "compile wpa_supplicant"
+        if [ ! -d ./$target/ ];then
+            echo "unzip $target package"
+            tar -xf $target.tar.gz
+        fi
+        cp ./config/deconfig ./$target/wpa_supplicant/.config
+        cd ./$target/wpa_supplicant; make; cd -
+        mkdir -p build
+        cp $target/wpa_supplicant/wpa_supplicant ./build
+    fi
+}
+
+run()
+{
+    sudo ./build/wpa_supplicant -Dnl80211,wext -i wlxe84e060a11bd -c config/wpa-psk-tkip.conf -d
+}
 
 
+
+build
+run
